4636effbced4210da499b9e69a7105653182ed65
win10virtualbox <jingchengsuozhi.wy@gmail.com>
2017-05-18 23:59:01 -0400

    stealth

---
+++ mo/Turing/Evaluation/plot.sh
@@ -0,0 +1,7 @@
+set xlabel "instruction"
+set ylabel "percentage"
+set term post eps color solid enh
+set output "st_bzip2.eps"
+set xtics rotate
+set xtics rotate by -60
+plot "bzip2-0p_dist" using ($3*100):xticlabels(1) title "bzip2" w p lt 3 lw 2 pt 2 ps 2, "bzip2-50p_dist" using ($3*100):xticlabels(1) title "obfuscated bzip2" w p lt 2 lw 2 pt 1 ps 2
