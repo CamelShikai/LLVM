4535b6725122aac02c7238d1136fa3ce7b1e3949
sk <jingchengsuozhi.wy@gmail.com>
2017-05-19 20:33:46 -0400

    finish stealth

--- mo/Turing/Evaluation/plot.sh
+++ mo/Turing/Evaluation/plot.sh
@@ -4,4 +4,4 @@ set term post eps color solid enh
 set output "st_bzip2.eps"
 set xtics rotate
 set xtics rotate by -60
-plot "bzip2-0p_dist" using ($3*100):xticlabels(1) title "bzip2" w p lt 3 lw 2 pt 2 ps 2, "bzip2-50p_dist" using ($3*100):xticlabels(1) title "obfuscated bzip2" w p lt 2 lw 2 pt 1 ps 2
+plot "bzip2-0p_dist" using ($3*100):xticlabels(1) title "bzip2" w lp lt 1 lw 2 pt 2 ps 2, "bzip2-50p_dist" using ($3*100):xticlabels(1) title "obfuscated bzip2" w lp lt 2 lw 2 pt 2 ps 2
--- mo/Turing/securecomm17/auto/lnicst.el
+++ mo/Turing/securecomm17/auto/lnicst.el
@@ -18,7 +18,8 @@
      "Figure 1"
      "Figure 2"
      "Figure 3"
-     "Figure 4")
+     "Figure 4"
+     "Figure 5")
     (TeX-run-style-hooks
      "url"
      "makeidx"
